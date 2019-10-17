b4903d2562b1783b8bd11a8c0a7ecb5a72775ec0
Phumlani <p.gwamanda@gmail.com>
2019-10-17 19:30:09 +0200

    home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { Component} from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+// Components
+import Navbar from './components/Navbar';
 
 // PAges
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+import { MuiThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#35baf6',
+      main: '#03a9f4',
+      dark: '#0276aa',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffa733',
+      main: '#ff9100',
+      dark: '#b26500',
+      contrastText: '#000',
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
         <Router>
-          <Switch>
-            <Route path="/" exact component={home} />
-            <Route path="/login" exact component={login} />
-            <Route path="/signup" exact component={signup} />
-          </Switch>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route path="/" exact component={home} />
+              <Route path="/login" exact component={login} />
+              <Route path="/signup" exact component={signup} />
+            </Switch>
+          </div>
         </Router>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/Scream.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+// Mui imports 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { Typography } from '@material-ui/core';
+
+const styles = {
+    card: {
+        display: 'flex'
+    }
+}
+
+class Scream extends Component {
+    render() {
+        const { classes, scream : { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount} } = this.props;
+        return (
+            <Card>
+                <CardMedia image={userImage} title="Profile image" />
+                <CardContent>
+                    <Typography variant="h5">{userHandle}</Typography> 
+                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography> 
+                    <Typography variant="body1">{body}</Typography> 
+                </CardContent>
+            </Card>
+        )
+    }
+}
+
+export default withStyles(styles)(Scream)
